b0e01f6e3f00a682a87699569f2b3985997832b7
ivanrfari <ivanrfari@gmail.com>
2026-07-20 19:21:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE = 'laslenas-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'rutas-lenas-v1';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+// Cuando se instala la app, guardamos lo básico
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(
-      fetch(e.request).catch(() => caches.match('./index.html'))
-    );
-  }
+// Cuando la app pide datos, intenta cargarlos normalmente
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
 });
